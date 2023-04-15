1daa8247361220876f9382e41cc65d3a49f5137a
ianlee-oakdale <124085958+ianlee-oakdale@users.noreply.github.com>
2023-04-15 17:57:38 -0400

    Add files via upload

---
+++ python_file_test.py
@@ -0,0 +1,24 @@
+import requests
+
+# API key for OpenWeatherMap
+API_KEY = "725f72d3953a51c26fcddf41dceb5bde"
+
+# API endpoint for Toronto weather
+url = "http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=" + API_KEY
+
+# Get weather information for Toronto
+response = requests.get(url)
+data = response.json()
+
+# Get the temperature, weather description, and weather icon
+temp = data['main']['temp']
+weather_description = data['weather'][0]['description']
+weather_icon = data['weather'][0]['icon']
+
+cel=int(temp)-273
+
+ww_temp=(str(cel)+chr(176)+' C')
+ww_descript=weather_description.title()
+
+pyscript.write(temp)
+pyscript.write(weather_description)
